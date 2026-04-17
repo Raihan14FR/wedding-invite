55c483fd13b8f6525a4b4c1fb14312d327c3f124
Raihan Fajar Rizqy <130721197+Raihan14FR@users.noreply.github.com>
2026-04-17 08:48:42 +0700

    Delete js/scroll.js

--- js/scroll.js
+++
@@ -1,5 +0,0 @@
-const progressBar = document.getElementById('scroll-progress');
-window.addEventListener('scroll', () => { const winScroll = document.documentElement.scrollTop; const height = document.documentElement.scrollHeight - window.innerHeight; const scrolled = (winScroll / height) * 100; progressBar.style.width = scrolled + '%'; });
-const aosElements = document.querySelectorAll('[data-aos]');
-const observer = new IntersectionObserver((entries) => { entries.forEach(entry => { if (entry.isIntersecting) { entry.target.classList.add('aos-animate'); observer.unobserve(entry.target); } }); }, { threshold: 0.2, rootMargin: "0px 0px -50px 0px" });
-aosElements.forEach(el => observer.observe(el));
